2a9e476404e2f6495be7b084f3d81eae9b47d388
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-01-04 23:46:24 +0900

    edit

--- 백준/17406_배열 돌리기 4_210104.py
+++ 백준/17406_배열 돌리기 4_210104.py
@@ -5,15 +5,15 @@ from itertools import permutations
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-
+# 회전
 def rotate(x, y, s):
     if s == 0:
         return
-    for i in range(4):
+    temp = a1[x][y]
+    for dir in range(4):
         for _ in range(s * 2):
-            ox, oy = x, y
-            x, y = x + dx[i], y + dy[i]
-            a2[x][y] = a1[ox][oy]
+            x, y = x + dx[dir], y + dy[dir]
+            temp, a1[x][y] = a1[x][y], temp
     rotate(x + 1, y + 1, s - 1)
 
 
@@ -21,12 +21,11 @@ n, m, k = map(int, input().split())
 a0 = [list(map(int, input().split())) for _ in range(n)]
 info = [list(map(int, input().split())) for _ in range(k)]
 ans = sys.maxsize
-for order in permutations(info, k):
+
+for option in permutations(info, k):
     a1 = copy.deepcopy(a0)
-    for r, c, s in order:
-        a2 = copy.deepcopy(a1)
+    for r, c, s in option:
         rotate(r - (s + 1), c - (s + 1), s)
-        a1 = a2
     for row in range(n):
         ans = min(ans, sum(a1[row]))
 print(ans)
