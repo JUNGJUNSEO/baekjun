c039c2143ec2367ff4ede143ae33fd14e720af3b
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-05-25 10:35:02 +0900

    Update 15961_회전 초밥_220413.py

--- 백준/15961_회전 초밥_220413.py
+++ 백준/15961_회전 초밥_220413.py
@@ -1,4 +1,7 @@
+import sys
 from collections import defaultdict
+input = sys.stdin.readline
+
 n, d, k, c = map(int, input().split())
 a = [int(input()) for _ in range(n)]
 a += a
@@ -6,22 +9,20 @@ left, right = 0, 0
 ans = 0
 length = len(a)
 d = defaultdict(int)
-s = set()
 
 while right < length:
 
     if right-left < k:
         d[a[right]] += 1
-        s.add(a[right])
         right += 1
     else:
-        if c not in s:
-            ans = max(ans, len(s)+1)
+        if c not in d:
+            ans = max(ans, len(d)+1)
         else:
-            ans = max(ans, len(s))
+            ans = max(ans, len(d))
         d[a[left]] -= 1
         if d[a[left]] == 0:
-            s.remove(a[left])
+            del d[a[left]]
         left += 1
 
 print(ans)
