843191a080db47963af3769f2fce65791cd1e1fa
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-05-31 22:43:00 +0900

    Create 2615_오목_220529.py

---
+++ 백준/2615_오목_220529.py
@@ -0,0 +1,36 @@
+dx = [0, 1, -1, 1]
+dy = [1, 0, 1, 1]
+
+
+def check(x, y, s, idx):
+    lst = list()
+    ox, oy = x, y
+    cnt = 1
+    while True:
+        x, y = x+dx[idx], y+dy[idx]
+        if 0 <= x < 19 and 0 <= y < 19:
+            if a[x][y] == s and not visit[x][y][idx]:
+                visit[x][y][idx] = True
+                cnt += 1
+            else:
+                break
+        else:
+            break
+    if cnt == 5:
+        return True
+
+
+a = [list(map(int, input().split())) for _ in range(19)]
+visit = [[[False]*4 for _ in range(19)] for _ in range(19)]
+
+for y in range(19):
+    for x in range(19):
+        for i in range(4):
+            if a[x][y] != 0 and not visit[x][y][i]:
+                visit[x][y][i] = True
+                if check(x, y, a[x][y], i):
+                    print(a[x][y])
+                    print(x+1, y+1)
+                    exit()
+
+print(0)
