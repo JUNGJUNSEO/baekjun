fadf5e01d5e51188939709df6f16b33925325454
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-03 18:59:48 +0900

    Update 17276_배열 돌리기_220601.py

--- 백준/17276_배열 돌리기_220601.py
+++ 백준/17276_배열 돌리기_220601.py
@@ -1,4 +1,9 @@
-import copy
+import sys
+input = sys.stdin.readline
+dx_p = [0, 1, 0, -1]
+dy_p = [1, 0, -1, 0]
+dx_m = [1, 0, -1, 0]
+dy_m = [0, 1, 0, -1]
 
 t = int(input())
 
@@ -6,19 +11,21 @@ for _ in range(t):
     n, d = map(int, input().split())
     a = [list(map(int, input().split())) for _ in range(n)]
     d //= 45
+    x, y, l = 0, 0, n//2
+    if d > 0:
+        dx, dy = dx_p, dy_p
+    else:
+        dx, dy = dx_m, dy_m
 
-    for _ in range(abs(d) % 8):
-        res = copy.deepcopy(a)
-
-        for i in range(n):
-            if d > 0:
-                res[i][n//2], res[i][n-1-i], res[n//2][n-1-i], res[n-1 -
-                                                                   i][n-1-i] = a[i][i], a[i][n//2], a[i][n-1-i], a[n//2][n-1-i]
-            else:
-                res[i][i], res[i][n//2], res[i][n-1-i], res[n//2][n-1 -
-                                                                  i] = a[i][n//2], a[i][n-1-i], a[n//2][n-1-i], a[n-1-i][n-1-i]
-
-        a = copy.deepcopy(res)
+    for _ in range(abs(d)):
+        for s in range(n//2):
+            nx, ny, nl = x+s, y+s, l-s
+            temp = a[nx][ny]
+            for i in range(4):
+                for _ in range(2):
+                    nx, ny = nx+dx[i]*nl, ny+dy[i]*nl
+                    a[nx][ny], temp = temp, a[nx][ny]
+        x, y, l = 0, 0, n//2
 
     for i in range(n):
         print(' '.join(map(str, a[i])))
