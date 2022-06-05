8902e864b5a881f59f249b29b90a844f4cac5f29
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-06-06 01:06:02 +0900

    Update 1713_후보 추천하기_220511.py

--- 백준/1713_후보 추천하기_220511.py
+++ 백준/1713_후보 추천하기_220511.py
@@ -1,37 +1,19 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
-
-
-def delete(MIN):
-    lst = list()
-    for key, value in student.items():
-        if value == MIN:
-            lst.append([order[key], key])
-    lst.sort()
-
-    del student[lst[0][1]]
-    del order[lst[0][1]]
 
 
 n = int(input())
 m = int(input())
 a = list(map(int, input().split()))
-student = defaultdict(int)
-order = dict()
+photo = dict()
 
 for i in range(m):
-    if len(student) < n:
-        student[a[i]] += 1
-        if a[i] not in order:
-            order[a[i]] = i
-    else:
-        if a[i] in student:
-            student[a[i]] += 1
-        else:
-            MIN = min(student.values())
-            delete(MIN)
-            student[a[i]] += 1
-            order[a[i]] = i
 
-print(' '.join(map(str, sorted(student.keys()))))
+    if a[i] in photo:
+        photo[a[i]][0] += 1
+    else:
+        if len(photo) == n:
+            del photo[a[sorted(photo.values())[0][1]]]
+        photo[a[i]] = [1, i]
+
+print(' '.join(map(str, sorted(photo.keys()))))
