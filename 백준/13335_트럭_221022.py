0067f02ef9d15d407f453ffffb1c1698942a2ae1
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-10-23 19:56:40 +0900

    Create 13335_트럭_221022.py

---
+++ 백준/13335_트럭_221022.py
@@ -0,0 +1,27 @@
+from collections import deque
+n, w, l = map(int, input().split())
+a = list(map(int, input().split()))
+s = deque([0]*w)
+idx = 0
+ans = 1
+
+while True:
+
+    s.popleft()
+
+    if sum(s) + a[idx] <= l:
+        s.append(a[idx])
+        idx += 1
+    else:
+        s.append(0)
+
+    if idx == n:
+        break
+
+    ans += 1
+while sum(s):
+    s.append(0)
+    s.popleft()
+    ans += 1
+
+print(ans)
