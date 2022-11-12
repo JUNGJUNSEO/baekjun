bda113da95ecbf592239fc4963deaed2a30c7c92
JUNGJUNSEO <88026797+JUNGJUNSEO@users.noreply.github.com>
2022-11-12 23:31:27 +0900

    Update 15486_퇴사 2_221111.py

--- 백준/15486_퇴사 2_221111.py
+++ 백준/15486_퇴사 2_221111.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 dp = [0] * (n + 1)
 
@@ -5,8 +8,7 @@ for i in range(1, n+1):
     t, p = map(int, input().split())
 
     if i+t-1 <= n:
-        dp[i+t-1] = max(dp[i+t-1], dp[i-1] + p)
+        dp[i+t-1] = max(dp[i+t-1], max(dp[:i]) + p)
 
-    dp[i] = max(dp[i], dp[i-1])
 
 print(max(dp))
